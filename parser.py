76a4e835e2e7ffbbf1dc5d999cbe57c3a9e2fd87
winter <q916090796@gmail.com>
2020-11-23 16:44:27 +0800

    add encoder and move the function of encoder and decoder into parser

--- parser.py
+++ parser.py
@@ -5,29 +5,36 @@ import base64
 class Parser(object):
     def __init__(self):
         super().__init__()
-        self.decode_func={}
-        self.decode_func[b'i']=self.decode_int
-        self.decode_func[b'd']=self.decode_dict
-        self.decode_func[b'l']=self.decode_list
-        self.decode_func[b'0']=self.decode_string
-        self.decode_func[b'1']=self.decode_string
-        self.decode_func[b'2']=self.decode_string
-        self.decode_func[b'3']=self.decode_string
-        self.decode_func[b'4']=self.decode_string
-        self.decode_func[b'5']=self.decode_string
-        self.decode_func[b'6']=self.decode_string
-        self.decode_func[b'7']=self.decode_string
-        self.decode_func[b'8']=self.decode_string
-        self.decode_func[b'9']=self.decode_string
+        self.decoder=Decoder()
+        self.encoder=Encoder()
 
-    def decode(self,file_path):
-        with open(file_path,'rb') as f:
-            bstr=f.read()
+    def decode(self,bstr):
+        return self.decoder.decode(bstr)
 
-        result,length=self.decode_func[bstr[0:1]](bstr,0)
+    def encode(self,data):
+        return self.encoder.encode(data)
 
-        with open('result.json','w') as f:
-            json.dump(result,f,indent=4)
+
+class Decoder(object):
+    def __init__(self):
+        super().__init__()
+
+    def check_type(self,bstr):
+        if bstr==b'i':
+            return self.decode_int
+        elif bstr==b'd':
+            return self.decode_dict
+        elif bstr==b'l':
+            return self.decode_list
+        elif bstr in b'0123456789':
+            return self.decode_string
+
+    def decode(self,bstr):
+        '''
+        convert bstr to bstr
+        '''
+        result,length=self.check_type(bstr[0:1])(bstr,0)
+        return result
 
     def decode_int(self,bstr,f):
         f+=1
@@ -39,7 +46,7 @@ class Parser(object):
     def decode_string(self,bstr,f):
         colon=bstr.index(b':',f)
         length=int(bstr[f:colon])
-        string=str(bstr[colon+1:colon+length+1])
+        string=bstr[colon+1:colon+length+1]
 
         return string,colon+length+1
 
@@ -48,7 +55,7 @@ class Parser(object):
         l=[]
         
         while bstr[f:f+1]!=b'e':
-            item,f=self.decode_func[bstr[f:f+1]](bstr,f)
+            item,f=self.check_type(bstr[f:f+1])(bstr,f)
             l.append(item)
 
         return l,f+1
@@ -59,12 +66,63 @@ class Parser(object):
 
         while bstr[f:f+1]!=b'e':
             key,f=self.decode_string(bstr,f)
-            value,f=self.decode_func[bstr[f:f+1]](bstr,f)
+            value,f=self.check_type(bstr[f:f+1])(bstr,f)
             d[key]=value
 
         return d,f+1
 
+
+class Encoder(object):
+    def __init__(self):
+        super().__init__()
+
+    def encode(self,data):
+        bstr=self.check_type(data)(data)
+        return bstr
+
+    def check_type(self,data):
+        if type(data) is int:
+            return self.encode_int
+        elif type(data) is bytes:
+            return self.encode_string
+        elif type(data) is list:
+            return self.encode_list
+        elif type(data) is dict:
+            return self.encode_dict
+
+    def encode_int(self,data): 
+        bstr=bytes('i{}e'.format(str(data)),encoding='utf8')
+        return bstr
+
+    def encode_string(self,data):
+        bstr=bytes('{}:'.format(len(data)),encoding='utf8')+data
+        return bstr
+
+    def encode_list(self,data):
+        bstr=b'l'
+
+        for item in data:
+            bstr+=self.check_type(item)(item)
+
+        bstr+=b'e'
+        return bstr
+
+    def encode_dict(self,data):
+        bstr=b'd'
+
+        for key,value in data.items():
+            bstr+=self.check_type(key)(key)
+            bstr+=self.check_type(value)(value)
+
+        bstr+=b'e'
+        return bstr
+
+            
 if __name__ == '__main__':
-    dec=Parser()
-    d=dec.decode('ubuntu.torrent')
+    with open('ubuntu.torrent','rb') as f:
+        dec=Parser()
+        bstr=f.read()
+        d=dec.decode(bstr)
+        b=dec.encode(d)
+        print(b==bstr)
 
